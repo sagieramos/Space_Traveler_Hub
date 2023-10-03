437d027b1d9692dd80ef082f73c96a2aafd7fac4
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-10-03 15:15:28 +0100

    structure

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -2,11 +2,11 @@ import React from 'react';
 import NavMenuItem from './NavMenuItem';
 
 const NavLink = () => (
-  <div id="nav-link">
+  <ul id="nav-link">
     <NavMenuItem to="./">Rockets</NavMenuItem>
     <NavMenuItem to="/mission">Mission</NavMenuItem>
     <NavMenuItem to="/myprofile">Myprofile</NavMenuItem>
-  </div>
+  </ul>
 );
 
 export default NavLink;
