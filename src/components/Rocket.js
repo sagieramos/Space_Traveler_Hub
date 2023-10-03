320cb0961803ab761a4bb34c3033f0fb8841956e
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-10-03 17:38:04 +0100

    rocket fetch

---
+++ src/components/Rocket.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Rocket = props => {
+  return (
+    <div>Rocket</div>
+  )
+}
+
+Rocket.propTypes = {}
+
+export default Rocket
---
+++ src/redux/rocketSlice.js
@@ -0,0 +1,51 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const rocketsApiUrl = 'https://api.spacexdata.com/v4/rockets';
+
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(rocketsApiUrl);
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error('Failed to fetch rockets');
+    }
+  } catch (error) {
+    return rejectWithValue(error.message);
+  }
+});
+
+const initialState = {
+    rockets: [],
+    statusFetch: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+    statusAdd: 'idle',
+    statusRemove: 'idle',
+    error: null,
+  };
+
+  const rocketSlice = createSlice({
+    name: 'rockets',
+    initialState,
+    reducers: {
+      resetStatus: (state) => {
+        state.statusFetch = 'idle';
+        state.statusAdd = 'idle';
+        state.statusRemove = 'idle';
+        state.error = null;
+      },
+    },
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchRockets.pending, (state) => {
+          state.statusFetch = 'loading';
+        })
+        .addCase(fetchRockets.fulfilled, (state, action) => {
+          state.statusFetch = 'succeeded';
+          state.books = action.payload;
+        })
+    },
+  });
+
+  export const { resetStatus } = rocketSlice.actions;
+  export default rocketSlice.reducer;
