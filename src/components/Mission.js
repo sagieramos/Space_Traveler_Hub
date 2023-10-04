2e56a6c14aa0a33dc69edb2cd23d26bbd42657bd
Fombi-Favour <fombifavour@gmail.com>
2023-10-04 12:16:03 +0100

    Update Mission.js

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectMission, setMission } from '../redux/missisonSlice';
 
-const Mission = () => (
-  <div>Mission</div>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+  const { mission } = useSelector(selectMission);
+
+  useEffect(() => {
+    const fetchMission = () => {
+      fetch('https://api.spacexdata.com/v3/missions')
+        .then((res) => {
+          if (res.ok) return res.json();
+          throw new Error();
+        })
+        .then((data) => {
+          dispatch(setMission(data));
+        })
+        .catch((err) => err.message);
+    };
+    if (mission.length === 0) fetchMission();
+  }, [dispatch, mission.length]);
+
+  return (
+    <div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Mission</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th aria-label="action-button" />
+            </tr>
+          </thead>
+          <tbody>
+            {mission.map((item) => (
+              <tr key={item.mission_id}>
+                <td data-testid={item.mission_id}>
+                  {item.mission_name}
+                </td>
+                <td>
+                  {item.description}
+                </td>
+                <td>
+                  A member
+                </td>
+                <td>
+                  Button
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Mission;
