a6c1611703f691a881b9ba7d3602bb0786642d6c
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-10-04 20:19:32 +0100

    Reserved toggle

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,40 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchRockets } from '../redux/rocketSlice';
+import { fetchRockets, toggleReserve } from '../redux/rocketSlice';
 import '../styles/rocket.scss';
-
-const getRandomNumber = (num) => {
-  const randomDecimal = Math.random();
-  const randomNumber = Math.floor(randomDecimal * (num));
-  return randomNumber;
-};
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.rockets);
-  const [dataFetched, setDataFetched] = useState(false);
 
   useEffect(() => {
-    if (!dataFetched) {
-      dispatch(fetchRockets());
-      setDataFetched(true);
-    }
-  }, [dispatch, dataFetched]);
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
+  const handleToggleReserve = (rocketId) => {
+    dispatch(toggleReserve(rocketId));
+  };
 
   return (
     <main>
       {rockets.map((rocket) => {
         const {
-          flickr_images: img, name, description, active, id,
+          flickr_images: img, name, description, reserve, id,
         } = rocket;
+
+        const reserved = reserve ? <span>Reserved</span> : null;
+
         return (
           <article key={id}>
-            <img src={img[getRandomNumber(img.length)]} alt="Rocket" />
+            <img src={img[0]} alt="Rocket" />
             <section>
               <h2>{name}</h2>
-              <p>{description}</p>
-              <button className={active ? 'reserved' : 'no-reserved'} type="button">
-                {active ? 'Reserve Rocket' : 'Cancel Reservation'}
+              <p>
+                {reserved}
+                {description}
+              </p>
+              <button
+                onClick={() => handleToggleReserve(id)}
+                className={reserve ? 'no-reserved' : 'reserved'}
+                type="button"
+              >
+                {reserve ? 'Cancel Reservation' : 'Reserve Rocket'}
               </button>
             </section>
           </article>
